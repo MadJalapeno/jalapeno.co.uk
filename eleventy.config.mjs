4861262085a34ba89f30081321a96d1327b9b7cc
MadJalapeno <github@jalapeno.co.uk>
2025-03-25 16:52:24 -0400

    Add new markdown pages for Homelab and S3 storage providers; remove outdated image and backup files; update package dependencies

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,10 +1,12 @@
 const now = String(Date.now())
 
-// Image transform plugin
-import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
-
 // Import the parse function from csv-parse
 import { parse } from "csv-parse/sync"; 
+
+// Import 11ty plugins
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 // add id to headings
 import markdownIt from "markdown-it";
@@ -36,12 +38,18 @@ export default function (eleventyConfig) {
   eleventyConfig.addGlobalData("env", process.env.ELEVENTY_ENV);
 
   // Plug Ins
+
+  // Image Plugin
   eleventyConfig.addPlugin(eleventyImageTransformPlugin,{
     formats: ["avif", "webp", "jpeg"],
   });
 
   // Embed SVGs
   eleventyConfig.addPlugin(svgContents);
+
+  // 11ty Plugins
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   // Automatically create a collection of all posts
   eleventyConfig.addCollection("postsDir", function (collectionApi) {
---
+++ src/pages/homelab/servers.md
@@ -0,0 +1,5 @@
+---
+title: Homelab Set Up
+parent: Homelab
+excerpt: What is consuming 82 watts in my basement
+---
